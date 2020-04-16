507c7d666acb4e449c6ab57167ef336a27250b10
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-04-16 16:39:54 +0200

    builded

---
+++ front/build/precache-manifest.140b8b4436fb11b06f0db06aee755d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80a028daa413032f7ec1aa6c0b400d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db42f766fda31b4cf2f2",
+    "url": "/static/css/main.f7bb847f.chunk.css"
+  },
+  {
+    "revision": "9425a1c1836d15bb55ac",
+    "url": "/static/js/2.23dcae33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.23dcae33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db42f766fda31b4cf2f2",
+    "url": "/static/js/main.a87cdf46.chunk.js"
+  },
+  {
+    "revision": "cd5cc0b019c8434d78aa",
+    "url": "/static/js/runtime-main.95a77404.js"
+  },
+  {
+    "revision": "56adfe899d5e3b9b4b58614d515bdfeb",
+    "url": "/static/media/budget-calc-background.56adfe89.png"
+  }
+]);
